cdef3a7abe02c0c77ce44de1cc52a4f95c34268b
Léo Carvalho <carvalho.csleo@gmail.com>
2022-03-17 13:06:30 -0300

    Implementa middlewares de sessão e cookies

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const homeController = require('./controller/homeController');
 const loginController = require('./controller/loginController');
+const estaLogado = require('./middlewares/estaLogado');
 const app = express();
 const port = 3000
 
@@ -10,10 +13,27 @@ app.use(express.urlencoded({ extended: true }));
 // set the view engine to ejs
 app.use('/', express.static('views'));
 app.set('view engine', 'ejs');
+app.use(cookieParser('xc bvhzksfgvbhgjbmknjlhdbfgkjfdnmklfdngjhdsbkeyboard cat', {
+    httpOnly: true,
+    secure: false,
+    expires: new Date('2022-03-10T00:05:00'),
+    maxAge: 300
+}));
+
+app.use(session({
+    secret: 'xc bvhzksfgvbhgjbmknjlhdbfgkjfdnmklfdngjhdsbkeyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    name: 'vb zdfgkljh mtyçjl,grenhkjdvui',
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    }
+}));
 
 app.use('/', loginController);
 
-app.use('/home', homeController)
+app.use('/home', estaLogado, homeController)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
