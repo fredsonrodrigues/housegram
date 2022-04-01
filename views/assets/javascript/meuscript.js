386d5ef665a483bc95df791199eb3ab449ee31da
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2022-03-31 21:56:28 -0300

    feat: add validation on login

--- views/assets/javascript/meuscript.js
+++ views/assets/javascript/meuscript.js
@@ -1,3 +1,28 @@
 const input = document.querySelector('input#username');
 
 input.classList.add('passou-por-cima');
+
+window.onload = function () {
+    const button = document.querySelector('button#login-button');
+    button.disabled = false;
+}
+
+function validateInput() {
+    const user = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log({user, password})
+    if (user !== "" && password !== "") {
+        const button = document.querySelector('button#login-button');
+        button.disabled = false;
+    }
+}
+
+function validateForm(event) {
+    const user = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    console.log({user, password})
+    if (user === "" && password === "") {
+        alert("Você precisa preencher os campos.")
+        event.preventDefault();
+    }
+}
